e6d2fd55ea45bd096a037dd406e85d2c5b441bf6
chsakell <chsakell@gmail.com>
2018-10-07 13:53:17 +0300

    Add project files.

---
+++ ssr-spa-navigation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ssr_spa_navigation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
